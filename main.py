6cfbb0113fcb928b3dfdc7dfa863aff0106c9c07
Fawz-Haaroon <fawzhaaroon216@gmail.com>
2024-08-02 14:21:46 +0530

    commit

--- main.py
+++ main.py
@@ -58,3 +58,13 @@ Python is mouse
 Python is mouse
 '''
 
+import turtle
+t = turtle.Turtle()
+turtle.bgcolor("black")
+t.speed(0)
+colors = ["red", "yellow", "blue", "green", "purple", "orange"]
+for i in range(300):
+    t.pencolor(colors[i % 6])
+    t.forward(i * 2)
+    t.right(61)
+turtle.done()
