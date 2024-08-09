11990c1b2d690a9cc5697e70d9f9f3e4cd027f78
Fawz-Haaroon <fawzhaaroon216@gmail.com>
2024-08-09 05:52:48 +0530

    yessir

--- main.py
+++ main.py
@@ -58,17 +58,7 @@ Python is mouse
 Python is mouse
 '''
 
-
-import turtle
-t = turtle.Turtle()
-turtle.bgcolor("black")
-t.speed(0)
-colors = ["red", "yellow", "blue", "green", "purple", "orange"]
-for i in range(1000):
-    t.pencolor(colors[i % 6])
-    t.forward(i * 1)
-    t.right(90)
-turtle.done()
+# 10 lines of ython code to your rescue
 
 import turtle
 t = turtle.Turtle()
