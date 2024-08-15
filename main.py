b36b21f0532a38e87c52bb5718fc012a2b61b10b
Fawz-Haaroon <fawzhaaroon216@gmail.com>
2024-08-15 15:59:15 +0530

    str

--- main.py
+++ main.py
@@ -128,7 +128,163 @@ True
 True
 '''
 
+#STRING-OPERATORS:- Escape Sequence Operator
+# some escape characters include \n,\b,\t,\\,\ooo,\xhh,
+# \a,\f,\r,\v
+print("I am Jaspreet and \nI am \x66rom \Indi\141\".")
+#Output
+'''
+I am Jaspreet and 
+I am from \India".
+'''
 
+#STRING-OPERATORS:- Formatting Operator (format specifiers)
+'''
+String formatting operator (%) is used to format a string
+%d, %c, %s, %f are some commonly used string formatters
+'''
+age = 28
+print('My age is %d' %(age))
+#Output
+'''
+My age is 28
+'''
+
+#String slicing with the THIRD parameter
+user = 'I am jaspreet'
+print(user[0:8:2])
+# Output
+'''
+'Ia a'
+'''
+
+s = 'abc' * 3
+print(s)
+print(s[::3])
+print(s[1::3])
+print(s[:5:3])
+#Output
+'''
+abcabcabc
+aaa
+bbb
+aa
+'''
+
+# Negative THIRD parameter
+s = 'I am Jaspreet'
+print(s[8:1:-2])
+#Output
+'''
+pa a
+'''
+# reversing a string
+print(s[::-1])
+#output
+'''
+teerpsaJ ma I
+'''
+
+# String Interpolation or String formatting
+'''
+the process of inserting an object into a predefined string is called string interpolation or string formatting
+'''
+# multiple variable interpolation
+name = 'fawz'
+city = 'chennai'
+print("My name is %s and I live in %s." % (name,city))
+#Output
+'''
+My name is fawz and I live in chennai.
+'''
+
+# str.format() function
+name = 'Fawz'
+city = 'Chennai'
+print("My name is {} and I live in {}".format(name,city))
+#Output
+'''
+My name is Fawz and I live in Chennai
+'''
+
+# Referencing variables though indexing
+name = 'Fawz'
+city = 'chennai'
+print("My name is {0} and I live in {1}".format(name, city))
+#Output
+'''
+My name is Fawz and I live in chennai
+'''
+
+# using keywords to refer for improving readability
+n = 'Fawz'
+c = 'Chennai'
+print("My name is {name} and I live in {city}".format(name=n,city=c))
+#Output
+'''
+My name is Fawz and I live in Chennai
+'''
+
+print("I got {0:f}% marks {2:f}% marks and {1:f}% in each term on English.".format(55.66,75.7768978989,56.99))
+#Output
+'''
+I got 55.660000% marks in English.
+'''
+
+print("I GOT {0:.5f} in english , {1:.7f} in maths".format(94.566,98.45656767867867878))
+#output
+'''
+I GOT 94.56600 in english , 98.4565677 in maths
+'''
+
+#fstrings
+name = 'Fawz'
+city = 'chennai'
+print(f"My name is {name} and I am {city}")
+print(F"My name is {name} and I am {city}")
+#Output
+'''
+My name is Fawz and I am chennai
+My name is Fawz and I am chennai
+'''
+
+#Calling methods of string is also possible
+name = "FAWZ"
+city = "Delhi"
+print(f'My name is {name.upper()} and I live in {city.upper()}')
+#output
+'''
+My name is FAWZ and I live in DELHI
+'''
+
+# for better readability multiline f-strings can be used
+name = 'FAWZ'
+age = 34
+gender = 'male'
+# 1st way - using backslash
+intro1 = f"my name is {name}." \
+                 f"my age is {age}." \
+                 f"my gender is {gender}."
+# 2nd way - using brackets
+intro2 =( f"my name is {name}."
+                 f"my age is {age}."
+                 f"my gender is {gender}.")
+# 3rd way - using triple quotes (everything including newlines and spaces are preserved in the format when this method is used)
+intro3 = f'''my name is {name}
+my age is {age}
+my gender is {gender}'''
+
+print(intro1)
+print(intro2)
+print(intro3)
+#Output
+'''
+my name is FAWZ.my age is 34.my gender is male.
+my name is FAWZ.my age is 34.my gender is male.
+my name is FAWZ
+my age is 34
+my gender is male
+'''
 
 
  
